9aec2f480a8bab9d105ea7eaec59ee499c9cc97a
Marcel Riedel <Riedel-Marcel@gmx.de>
2022-10-06 13:49:24 +0200

    move dai-specific font options to publications theme

--- PublicationsThemePlugin.inc.php
+++ PublicationsThemePlugin.inc.php
@@ -10,8 +10,88 @@ class PublicationsThemePlugin extends ThemePlugin {
 
         // Use the parent theme's unique plugin slug
         $this->setParent('defaultthemeplugin');
+
+        // Register theme options
+        $this->addOption('typography', 'FieldOptions', [
+            'type' => 'radio',
+            'label' => __('plugins.themes.default.option.typography.label'),
+            'description' => __('plugins.themes.default.option.typography.description'),
+            'options' => [
+                [
+                    'value' => 'notoSans',
+                    'label' => __('plugins.themes.default.option.typography.notoSans'),
+                ],
+                [
+                    'value' => 'notoSerif',
+                    'label' => __('plugins.themes.default.option.typography.notoSerif'),
+                ],
+                [
+                    'value' => 'notoSerif_notoSans',
+                    'label' => __('plugins.themes.default.option.typography.notoSerif_notoSans'),
+                ],
+                [
+                    'value' => 'notoSans_notoSerif',
+                    'label' => __('plugins.themes.default.option.typography.notoSans_notoSerif'),
+                ],
+                [
+                    'value' => 'lato',
+                    'label' => __('plugins.themes.default.option.typography.lato'),
+                ],
+                [
+                    'value' => 'lora',
+                    'label' => __('plugins.themes.default.option.typography.lora'),
+                ],
+                [
+                    'value' => 'lora_openSans',
+                    'label' => __('plugins.themes.default.option.typography.lora_openSans'),
+                ],
+                [
+                    'value' => 'cargan_openSans',
+                    'label' => __('plugins.themes.default.option.typography.cargan_openSans'),
+                ],
+            ],
+            'default' => 'notoSans',
+        ]);
+
+        // Store additional LESS variables to process based on options
+        $additionalLessVariables = array();
+
+        if ($this->getOption('typography') === 'notoSerif') {
+            $this->addStyle('font', 'styles/fonts/notoSerif.less');
+            $additionalLessVariables[] = '@font: "Noto Serif", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen-Sans", "Ubuntu", "Cantarell", "Helvetica Neue", sans-serif;';
+        } elseif (strpos($this->getOption('typography'), 'notoSerif') !== false) {
+            $this->addStyle('font', 'styles/fonts/notoSans_notoSerif.less');
+            if ($this->getOption('typography') == 'notoSerif_notoSans') {
+                $additionalLessVariables[] = '@font-heading: "Noto Serif", serif;';
+            } elseif ($this->getOption('typography') == 'notoSans_notoSerif') {
+                $additionalLessVariables[] = '@font: "Noto Serif", serif;@font-heading: "Noto Sans", serif;';
+            }
+        } elseif ($this->getOption('typography') == 'lato') {
+            $this->addStyle('font', 'styles/fonts/lato.less');
+            $additionalLessVariables[] = '@font: Lato, sans-serif;';
+        } elseif ($this->getOption('typography') == 'lora') {
+            $this->addStyle('font', 'styles/fonts/lora.less');
+            $additionalLessVariables[] = '@font: Lora, serif;';
+        } elseif ($this->getOption('typography') == 'lora_openSans') {
+            $this->addStyle('font', 'styles/fonts/lora_openSans.less');
+            $additionalLessVariables[] = '@font: "Open Sans", sans-serif;@font-heading: Lora, serif;';
+
+        // dai-specific: add Cargan font combinded with OpenSans
+        } elseif ($this->getOption('typography') == 'cargan_openSans') {
+            $this->addStyle('font', 'styles/fonts/cargan_openSans.less');
+            $additionalLessVariables[] = '@font: "Open Sans", sans-serif;@font-heading: Cargan, serif;';
+        } else {
+            $this->addStyle('font', 'styles/fonts/notoSans.less');
+        }
+
+        // Pass additional LESS variables based on options
+        if (!empty($additionalLessVariables)) {
+            $this->modifyStyle('stylesheet', array('addLessVariables' => join("\n", $additionalLessVariables)));
+        }
+
         $this->addScript('idaiworld-dropdown', 'js/idai-world.js');
         $this->modifyStyle('stylesheet', array('addLess' => array('styles/index.less')));
+
     }
 
    /**
